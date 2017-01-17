b3aa0801bce8dcbef20cbf0ae6f656d704e2d3e5
rickdean <rickdean@outlook.com>
2017-01-17 16:07:07 -0500

    Increased to version 1.3
    v1.3

--- public/vendor.js
+++ public/vendor.js
@@ -100,7 +100,7 @@
 /***/ function(module, exports, __webpack_require__) {
 
 	__webpack_require__(2);
-	module.exports = __webpack_require__(56);
+	module.exports = __webpack_require__(52);
 
 
 /***/ },
@@ -10332,7 +10332,7 @@
 
 /***/ },
 
-/***/ 56:
+/***/ 52:
 /***/ function(module, exports, __webpack_require__) {
 
 	/* WEBPACK VAR INJECTION */(function(jQuery) {/*!
